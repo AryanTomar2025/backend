67abdac12ee5bde45271b48b81c86df6d96af37c
Aryan <aryantomarcs@gmail.com>
2024-03-12 06:23:20 +0530

    apitest

--- src/app.js
+++ src/app.js
@@ -14,4 +14,17 @@ app.use(express.json({ limit: "16kb" })); // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+import userRouter from './routes/user.routes.js'
+
+
+app.use("/api/v1/users",userRouter)
+
+
+
+
+
+
+
 export { app };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcyrpt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -17,7 +18,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       lowercase: true,
@@ -47,5 +48,41 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcyrpt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcyrpt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_SECRET,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
